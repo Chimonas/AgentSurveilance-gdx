2e15b47ab972e0f9b4af80b2669f1553cfa0a734
MaaikeHHSTRA <maaikehooghiemstra@live.nl>
2019-05-22 11:27:25 +0200

    exploration

---
+++ core/src/com/mygdx/game/worldAttributes/agents/guard/explorationAi/InfoGainAlgo.java
@@ -0,0 +1,28 @@
+package com.mygdx.game.worldAttributes.agents.guard.explorationAi;
+
+import com.mygdx.game.gamelogic.GameLoop;
+import com.mygdx.game.worldAttributes.Pheromone;
+import com.mygdx.game.worldAttributes.agents.guard.Guard;
+import com.mygdx.game.worldAttributes.agents.guard.ai.GuardAI;
+
+public class InfoGainAlgo extends GuardAI {
+    public InfoGainAlgo(Guard guard) {
+        super(guard);
+    }
+
+    @Override
+    public void update(){
+        super.update();
+
+        if(Math.random() < 0.1f / GameLoop.TICKRATE)
+            guard.createPheromone(Pheromone.PheromoneType.values()[(int)(Math.random() * Pheromone.PheromoneType.values().length)]);
+    }
+
+    public float getNewAngle() {
+        return 0;
+    }
+
+    public float getNewVelocity() {
+        return 0;
+    }
+}
